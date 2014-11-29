db05c861068346fa35bcfd1044e75de2083b1f09
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-11-29 18:17:50 +0100

    Added ini properties to handle big file uploads

--- public/index.php
+++ public/index.php
@@ -7,6 +7,11 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['RE
     return false;
 }
 
+// Define file upload properties
+ini_set('post_max_size', '1536M');
+ini_set('upload_max_filesize', '1536M');
+ini_set('memory_limit', '1536M');
+
 // Run the application!
 $app = Zend\Mvc\Application::init([
     'modules' => [
